a0975eb49ca0a7900648392eb10c16c0e145aecc
chuchocoello <100742242+chuchocoello@users.noreply.github.com>
2022-12-09 15:51:41 -0600

    a little of style

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,59 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import './App.css'
-import LocationInfo from './components/LocationInfo'
-import ResidentCard from './components/ResidentCard'
-import getRandomNumber from './utils/getRandomNumber'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+import LocationInfo from './components/LocationInfo';
+import ResidentCard from './components/ResidentCard';
+import getRandomNumber from './utils/getRandomNumber';
 
 function App() {
-
-  const [location, setLocation] = useState()
+  const [location, setLocation] = useState();
 
   const getDataDimensions = (Dimension) => {
-    const url = `https://rickandmortyapi.com/api/location/${Dimension}`
-    axios.get(url)
-      .then(res => setLocation(res.data))
-      .catch(err => console.log(err))
-  }
+    const url = `https://rickandmortyapi.com/api/location/${Dimension}`;
+    axios
+      .get(url)
+      .then((res) => setLocation(res.data))
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
-    getDataDimensions(getRandomNumber())
-  }, [])
+    randomClick();
+  }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const dimensionSearch = event.target.serchValue.value;
+    getDataDimensions(dimensionSearch);
+  };
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    const dimensionSearch = event.target.serchValue.value
-    getDataDimensions(dimensionSearch)
-  }
+  const randomClick = () => {
+    getDataDimensions(getRandomNumber());
+  };
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input id='serchValue' type="text" placeholder="Search a dimention"/>
-        <button type='submit'>Serch</button>
-        <button>Random</button>
-      </form>
-
-      <h1>R&M Random Dimension: </h1>
-      <LocationInfo location={location} />
-      <section>
-        {
-          location?.residents.map(urlResident => ( <ResidentCard key={urlResident} urlResident={urlResident}/>
-          ))
-        }
+      <section className="header">
+        <div className="serch">
+        <h1>The Rick & Morty App</h1>
+        <form onSubmit={handleSubmit}>
+          <input id="serchValue" type="text" placeholder="Search a dimention" />
+          <button type="submit">Serch</button>
+        </form>
+        <button onClick={randomClick}>Random</button>
+        </div>
       </section>
+
+      <section className="info">
+      <h2>R&M Random Dimension: </h2>
+      <LocationInfo location={location} />
+      <div>
+        {location?.residents.map((urlResident) => (
+          <ResidentCard key={urlResident} urlResident={urlResident} />
+          ))}
+      </div>
+          </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
